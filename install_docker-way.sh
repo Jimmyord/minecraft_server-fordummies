65b1fea66d42bd607f4b06937eda7501c77da862
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-03-02 23:14:03 +0100

    Update install_docker-way.sh

--- install_docker-way.sh
+++ install_docker-way.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
-#TO DO : Ajout le contrôle systemctl enable de docker
-if [[ $(systemctl is-enabled docker | echo $? ) -eq 0 ]]
-then
-	echo "Docker is enabled."
-else
-	systemctl enable docker
-	if [[ $(echo $?) -eq 0 ]]
-	then
-		echo "Docker was inactive. That's ok now."
-	else
-		echo "Docker isn't installed. Please install it."
-		exit 1
-	fi
-fi
-#Starting the docker service if it is inactive
-if [[ $(systemctl is-active docker) = "active" ]]
-then
-	echo "Docker is running. Great!"
-else
-	echo "Docker isn't running. I'm starting it for you"
-	systemctl start docker
-fi
-#Test de la présence de docker-compose
-if [ -x /usr/local/bin/docker-compose ]
-then
-	echo "docker-compose is already installed"
-else 
-	echo "docker-compose needs to be on your system to go through, please install it."
-	exit 1
-fi
-read -p "Please enter the desired minecraft version." version
 version=$(curl https://minecraft.gamepedia.com/Version_history | sed -nr 's/^.*([1-9]+\.[0-9]+\.[0-9]+)" title="[1-9]+\.[1-9]+\.[1-9]+.*$/\1/p' | head -n1)
 sed -ri 's/^(.*versionminecraft:).*$/\1 $version/' docker-compose.yml
 #Now let's build the container
@@ -37,11 +6,7 @@ docker-compose up -d
 if [[ $? = 0 ]] 
 then
 	echo "Server is running."
-	echo "Save script :"
-	./save.sh
 else
 	echo "La mise en service du conteneur a rencontré un problème."
-	echo "A problem occured during the startup of the conatainer"
 	exit 1
 fi
-exit 0
