8a8a1a3a9e8a41fd193c2f1bfac46b14c0d56718
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-03-03 00:54:49 +0100

    Update install_docker-way.sh

--- install_docker-way.sh
+++ install_docker-way.sh
@@ -6,15 +6,17 @@ do
 version=$ligne
 sed -ri 's/^(.*versionminecraft:).*$/\1 '$version'/' docker-compose.yml
 #Now let's build the container
-docker-compose up --force-recreate -d 
+docker-compose up -d 
 if [[ $? = 0 ]] 
 then
 	echo "Server is running."
+	docker-compose down
 else
 	echo "La mise en service du conteneur a rencontré un problème."
 	exit 1
 fi
 docker tag serveurminecraft:latest jimmyord/minecraft_server:$version
+docker rmi serveurminecraft:latest
 #docker tag serveurminecraft:latest jimmyord/minecraft_server:last
 docker login -u $dockerlogin -p $dockerpassword
 docker push jimmyord/minecraft_server:$version
