f898d9430381890d54146568febda9e1f2633583
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-03-03 03:02:40 +0100

    Update install_docker-way.sh

--- install_docker-way.sh
+++ install_docker-way.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+docker login -u $dockerlogin -p $dockerpassword
 curl https://minecraft.gamepedia.com/Version_history | sed -nr 's/^.*([1-9]+\.[0-9]+\.[0-9]+)" title="[1-9]+\.[1-9]+\.[1-9]+.*$/\1/p' > minecraft.txt
 #Suppression des versions indisponibles
-sed -ir 's/1.2.[1-4]//g' minecraft.txt
+sed -ir '/^.*1.2.[1-4].*$/d' minecraft.txt
 while read ligne
 do
 #version=$(curl https://minecraft.gamepedia.com/Version_history | sed -nr 's/^.*([1-9]+\.[0-9]+\.[0-9]+)" title="[1-9]+\.[1-9]+\.[1-9]+.*$/\1/p' | head -n1)
@@ -20,7 +21,7 @@ fi
 docker tag serveurminecraft:latest jimmyord/minecraft_server:$version
 docker rmi serveurminecraft:latest
 #docker tag serveurminecraft:latest jimmyord/minecraft_server:last
-docker login -u $dockerlogin -p $dockerpassword
 docker push jimmyord/minecraft_server:$version
 #docker push jimmyord/minecraft_server:last
 done < <(cat minecraft.txt)
+docker logout
