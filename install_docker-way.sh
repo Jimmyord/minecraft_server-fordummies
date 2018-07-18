471f328eef62faa7cdbe680d34048f61e14e12b2
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-07-18 15:29:08 +0200

    Update install_docker-way.sh
    Set back the building way.

--- install_docker-way.sh
+++ install_docker-way.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-docker login -u $dockerlogin -p $dockerpassword
+#Available versions
 curl https://minecraft.gamepedia.com/Java_Edition_version_history | sed -nr 's/^.*([1-9]+\.[0-9]+\.[0-9]+)" title="[1-9]+\.[1-9]+\.[1-9]+.*$/\1/p' > minecraft.txt
-#Suppression des versions indisponibles
-sed -ir '/^.*1.2.[1-4].*$/d' minecraft.txt
-while read ligne
-do
-#version=$(curl https://minecraft.gamepedia.com/Version_history | sed -nr 's/^.*([1-9]+\.[0-9]+\.[0-9]+)" title="[1-9]+\.[1-9]+\.[1-9]+.*$/\1/p' | head -n1)
-version=$ligne
+sed -ir '/^.*1.2.[1-4].*$/d' minecraft.txt 1>&2>/dev/null
+echo 'Available Versions : '
+cat minecraft.txt
+read -p "Give me the version you want : " version
+#Building the container
 sed -ri 's/^(.*versionminecraft:).*$/\1 '$version'/' docker-compose.yml
-#Now let's build the container
 docker-compose up -d 
 if [[ $? = 0 ]] 
 then
@@ -18,10 +16,3 @@ else
 	echo "La mise en service du conteneur a rencontré un problème."
 	exit 1
 fi
-docker tag serveurminecraft:latest jimmyord/minecraft_server:$version
-docker rmi serveurminecraft:latest
-#docker tag serveurminecraft:latest jimmyord/minecraft_server:last
-docker push jimmyord/minecraft_server:$version
-#docker push jimmyord/minecraft_server:latest
-done < <(cat minecraft.txt)
-docker logout
