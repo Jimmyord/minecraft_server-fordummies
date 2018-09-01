7a103d66969b0718153644277849a9cfde3244ce
Jimmyord <32267874+Jimmyord@users.noreply.github.com>
2018-09-01 19:58:33 +0200

    Update DockerHub_Update.sh

--- DockerHub_Update.sh
+++ DockerHub_Update.sh
@@ -5,21 +5,27 @@ curl https://minecraft.gamepedia.com/Java_Edition_version_history | sed -nE 's/^
 sed -ir '/^.*1.2.[1-4].*$/d' minecraft.txt
 while read ligne
 do
-version=$ligne
-sed -ri 's/^(.*versionminecraft:).*$/\1 '$version'/' docker-compose.yml
-#Building and running the container
-docker-compose up -d 
-if [[ $? = 0 ]] 
+test=$(echo $ligne | sed -r 's/\./\\./g')
+if [ $ligne != $(curl https://hub.docker.com/r/jimmyord/minecraft_server/tags/ | sed -En 's/^.*($test).*$/\1/p') ]
 then
-	echo "Server is running."
-	docker-compose down
-else
-	echo "La mise en service du conteneur a rencontré un problème."
-	exit 1
-fi
-docker tag serveurminecraft:latest jimmyord/minecraft_server:$version
-docker rmi serveurminecraft:latest
-docker push jimmyord/minecraft_server:$version
+	version=$ligne
+	sed -ri 's/^(.*versionminecraft:).*$/\1 '$version'/' docker-compose.yml
+	#Building and running the container
+	docker-compose up -d 
+	if [[ $? = 0 ]] 
+	then
+		echo "Server is running."
+		docker-compose down
+	else
+		echo "La mise en service du conteneur a rencontré un problème."
+		exit 1
+	fi
+	docker tag serveurminecraft:latest jimmyord/minecraft_server:$version
+	docker rmi serveurminecraft:latest
+	docker push jimmyord/minecraft_server:$version
 #docker push jimmyord/minecraft_server:latest
-done < <(cat minecraft.txt)
-docker logout
+	done < <(cat minecraft.txt)
+	docker logout
+else
+	echo "$version existe déjà!"
+fi
